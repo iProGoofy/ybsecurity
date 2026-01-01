388e856ffb1bcf12e97fb454b821398d2f5f9042
iProGoofy <517292@student.fontys.nl>
2026-01-01 02:57:01 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.ybsecurity.nl" }],
-        destination: "https://ybsecurity.nl/:path*",
-        permanent: true,
-      },
-    ];
-  },
+
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-
-module.exports = nextConfig;
-
 
 export default nextConfig;
